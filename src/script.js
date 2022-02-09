2d0f179bdb3363befbf62ec6619cabd4633162a2
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-09 14:32:01 +0530

    Understanding Uninitialized Variables subtask 7

--- src/script.js
+++ src/script.js
@@ -16,3 +16,14 @@ var a = 9;
 var myFirstName = "ABC"
 var myLastName = "XYZ"
 //Declare String Variables subtask 6
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+//Understanding Uninitialized Variables subtask 7
