cd53a9b0e821bee24d914d0bdd5355acb48858bf
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-10 10:23:06 +0530

    Replacing If Else Chains with Switch

--- src/script.js
+++ src/script.js
@@ -594,6 +594,32 @@ switch(val) {
 
 sequentialSizes(1);
 //Multiple Identical Options in Switch Statements subtask 78
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+
+
+  case "bob" :
+    answer = "Marley";
+    break;
+  case 42 :
+    answer = "The Answer";
+    break;
+  case 1 :
+    answer = "There is no #1";
+    break;
+  case 99 :
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+  
+}
+  // Only change code above this line
+  return answer;
+}
+//Replacing If Else Chains with Switch subtask 77
 
 
 
