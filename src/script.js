0da91fb7dea4ff50ab349cf053673d0914995ad8
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-10 11:43:49 +0530

    Testing Objects for Properties

--- src/script.js
+++ src/script.js
@@ -778,3 +778,17 @@ function phoneticLookup(val) {
 
 phoneticLookup("charlie");
 //Using Objects for Lookups subtask 88
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp))
+  {
+  return obj[checkProp];
+  }
+  else
+
+   return "Not Found";
+
+  return "Change Me!";
+  // Only change code above this line
+}
+//Testing Objects for Properties subtask 89
