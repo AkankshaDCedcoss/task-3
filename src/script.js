3f4252cf4ae7ad35ccbe9ddea553253a17514903
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-10 11:07:57 +0530

    Accessing Object Properties with Variables subtask 84

--- src/script.js
+++ src/script.js
@@ -718,3 +718,13 @@ const testObj = {
 const entreeValue = testObj['an entree'];   // Change this line
 const drinkValue = testObj['the drink'];    // Change this line
 //Accessing Object Properties with Bracket Notation subtask 83
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+//Accessing Object Properties with Variables subtask 84
